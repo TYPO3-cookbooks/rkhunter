2ec72385a59f517711075b36d1a317d8529da800
Steffen Gebert <steffen.gebert@typo3.org>
2012-06-19 22:09:51 +0200

    Add cookbook rkhunter

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "TYPO3 Association"
+maintainer_email "steffen.gebert@typo3.org"
+license          "MIT"
+description      "Installs/Configures rkhunter"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
